0127b530129023aca9e427f82dc4295b1a484e65
Abrahamcepe <abrahamgolf@gmail.com>
2020-09-15 21:05:09 -0500

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from'./Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Helloww</h1>
+      {/* header */}
+      <Header/>
+      {/* nav */}
+      {/* results */}
     </div>
   );
 }
